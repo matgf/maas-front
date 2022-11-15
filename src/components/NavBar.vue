ab9fef3ef38c8d3531f04862a63b17d752fdad99
Matias Gaete <matiasgf18@gmail.com>
2022-11-15 19:24:08 -0300

    chore: change router

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,10 +10,10 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#">Shift Availability</a>
+            <a class="nav-link" href="#/confirmed-shifts">Confirmed Engineers </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Confirmed Engineers</a>
+            <a class="nav-link" href="#/availability-shifts">Shift Availability</a>
           </li>
         </ul>
       </div>
