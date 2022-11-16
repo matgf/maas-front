375a5bf47a628f72ba3f4bb1ef7b816951cf6e26
Matias Gaete <matiasgf18@gmail.com>
2022-11-16 00:52:52 -0300

    feat: added font awesome and icon

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,4 +15,8 @@ import store from './store';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).use(store).use(router).mount('#app');
+/* add icons to the library */
+library.add(faTriangleExclamation);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router)
+  .mount('#app');
