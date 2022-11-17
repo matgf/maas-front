56740d6916eda5feec9c755d31815c1dfc520a28
Matias Gaete <matiasgf18@gmail.com>
2022-11-16 22:18:15 -0300

    chore: added resetShifts and better sorting

--- src/views/ConfirmedView.vue
+++ src/views/ConfirmedView.vue
@@ -6,6 +6,8 @@ import moment from 'moment';
 
 const services = reactive({});
 const currentService = ref(0);
+const currentServiceName = ref('');
+const viewTable = ref(false);
 const datesShifts = reactive({});
 const engineers = reactive({});
 
@@ -71,7 +73,36 @@ const selectService = (e) => {
   }).then((result) => {
     datesShifts.value = result.data.data.service.orderedShifts;
     engineers.value = result.data.data.service.engineers;
+    currentServiceName.value = result.data.data.service.name;
+    viewTable.value = true;
     console.log(result.data.data.service.orderedShifts);
+  });
+};
+
+const resetShifts = () => {
+  axios({
+    url: 'http://localhost:3001/graphql',
+    method: 'post',
+    data: {
+      query: `
+        query($serviceId: ID!){
+                resetShifts(serviceId: $serviceId){
+                    id
+                    assigned
+                    engineerId
+
+            }
+        }
+    `,
+      variables: {
+        serviceId: currentService.value,
+      },
+    },
+  }).then((result) => {
+    viewTable.value = false;
+    currentServiceName.value = '';
+    currentService.value = 0;
+    datesShifts.value = null;
   });
 };
 const checkShiftEngineer = (engineerId, engineersService) => {
@@ -83,6 +114,7 @@ const checkShiftEngineer = (engineerId, engineersService) => {
 <template>
 
 <h1> Confirmed Shifts</h1>
+{{currentServiceName}}
 <!-- Service Dropdown -->
 <div class="btn-group">
   <button type="button" class="btn btn-success dropdown-toggle"
@@ -98,30 +130,35 @@ const checkShiftEngineer = (engineerId, engineersService) => {
     </li>
   </ul>
 </div>
-{{currentService}}
+
+  <button v-if='datesShifts.value' type="button" class="btn btn-success" :onClick='resetShifts'>
+    Reset Shifts
+  </button>
 
 <!-- Tables -->
-<table v-for="(date) in datesShifts.value" :key='date.dates'
-  class="table table-striped table-bordered table-hover">
-  <thead>
-    <tr>
-      <th :colSpan=2 scope="col">{{ date.dates}}</th>
+<div v-if="viewTable">
+  <table  v-for="(date) in datesShifts.value" :key='date.dates'
+    class="table table-striped table-bordered table-hover">
+    <thead>
+      <tr>
+        <th :colSpan=2 scope="col">{{ date.dates}}</th>
 
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="shift in date.shifts" :key='shift.id'>
-      <td scope="row">
-        {{moment(shift.startTime).format('HH:mm')}} - {{ moment(shift.endTime).format('HH:mm')}}
-      </td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="shift in date.shifts.sort((a, b) => a.id - b.id)" :key='shift.id'>
+        <td scope="row">
+          {{moment(shift.startTime).format('HH:mm')}} - {{ moment(shift.endTime).format('HH:mm')}}
+        </td>
 
-      <td>
-        <div v-if="shift.assigned"> {{checkShiftEngineer(shift.engineerId, engineers.value)}}</div>
-        <font-awesome-icon v-else :icon="['fas', 'triangle-exclamation']" />
-      </td>
-    </tr>
-  </tbody>
-</table>
+        <td>
+          <div v-if="shift.assigned && shift.engineerId"> {{checkShiftEngineer(shift.engineerId, engineers.value)}}</div>
+          <font-awesome-icon v-else :icon="['fas', 'triangle-exclamation']" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
 </template>
 
 <style>
